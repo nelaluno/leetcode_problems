be1904fba8eb4c75a16d9828812c2e61f337cb99
Anika Filippova <anikax.filippova@intel.com>
2025-12-02 15:53:39 +0200

    Time: 91 ms (29.84%), Space: 63.7 MB (26.19%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        y_alligned_points = defaultdict(int)
+        for x, y in points:
+            y_alligned_points[y] += 1
+        
+        line_counts = []
+        for x in y_alligned_points:
+            point_count = y_alligned_points[x]
+            if point_count > 1:
+                line_counts.append(point_count*(point_count -1)//2)
+
+        if len(line_counts) < 2:
+            return 0
+
+        result = 0
+        module = 10**9 + 7
+        summs = [0] * len(line_counts)
+        prev = 0
+        for ind in range(len(line_counts)-1, 0, -1):
+            summs[ind-1] = prev + line_counts[ind]
+            # if result > module:
+            #     result = result % module
+            prev = summs[ind-1]
+
+        print(summs)
+        for ind, count in enumerate(line_counts):
+            result += count * summs[ind]
+
+            if result > module:
+                result = result % module
+
+        return result
