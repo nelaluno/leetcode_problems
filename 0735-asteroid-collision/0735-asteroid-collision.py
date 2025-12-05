f24f0f7ed99fb1302cf14b0a460ea26630c71a1f
Anika Filippova <anikax.filippova@intel.com>
2025-12-05 16:36:47 +0200

    Time: 47 ms (5.94%), Space: 18.2 MB (99.97%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,30 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        for ind, ast in enumerate(asteroids):
+            ast_size = abs(ast)
+            if ast < 0: # left
+                # if we passed not collided asteroids moving right, 
+                # go through them from ind-1 to 0
+                # until ast_size > asteroids
+                for collision_ind in range(ind-1, -1, -1):
+                    if not asteroids[collision_ind]:
+                        continue
+                    
+                    if asteroids[collision_ind] < 0:
+                        break
+
+                    coll_size = abs(asteroids[collision_ind])
+                        
+                    if ast_size == coll_size:
+                        asteroids[collision_ind] = 0
+                        asteroids[ind] = 0
+                        break
+                    if ast_size < coll_size:
+                        asteroids[ind] = 0
+                        break
+
+                    asteroids[collision_ind] = 0
+            # else: #right
+            #     pass
+        
+        return [a for a in asteroids if a]
