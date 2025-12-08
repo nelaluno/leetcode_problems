d2bde6ac5a110aa698f3d59c43c0d31738be6f0f
Anika Filippova <anikax.filippova@intel.com>
2025-12-08 17:38:29 +0200

    Time: 54 ms (93.21%), Space: 18 MB (11.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -2,17 +2,12 @@ class Solution:
     def countTriples(self, n: int) -> int:
         squares = [n**2 for n in range(1, n+1)]
         squares_set = set(squares)
-        # print("squares", squares)
         count = 0
         for ind_a in range(n):
             for ind_b in range(ind_a+1, n):
-                # print("check", squares[ind_a], squares[ind_b])
                 sq_sum = squares[ind_a] + squares[ind_b]
                 if sq_sum > squares[-1]:
-                    # print(sq_sum, ">", squares[-1])
                     break
                 if sq_sum in squares_set:
-                    # (a, b) and (b, a)
                     count += 2
-                    # print(ind_a, ind_b)
         return count
