45b339b2f730612fb2d30977c68b8dad37dac093
Anika Filippova <anikax.filippova@intel.com>
2025-12-10 12:22:36 +0200

    Time: 12 ms (87.22%), Space: 31.8 MB (96.11%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -2,12 +2,13 @@ class Solution:
     def countPermutations(self, complexity: List[int]) -> int:
         mod = 10**9 + 7
 
-        decrypted_compl = complexity[0]           
+        decrypted_compl = complexity[0]
+        for compl in complexity[1:]:
+            if compl <= decrypted_compl:
+                return 0
 
         factorial = 1
         for i in range(1, len(complexity)):
-            if complexity[i] <= decrypted_compl:
-                return 0
             factorial *= i
             factorial = factorial % mod
         return factorial
