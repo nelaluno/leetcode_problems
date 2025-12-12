a651d1bbee44ca772983431a508e4dc2b2695a84
Anika Filippova <anikax.filippova@intel.com>
2025-12-12 16:31:07 +0200

    Time: 0 ms (100%), Space: 17.6 MB (98.3%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -7,39 +7,25 @@ class Solution:
         m_min, m_max = 0, m-1
         n_min, n_max = 0, n-1
         while m_min <= m_max and n_min <= n_max:
-            # print("round")
-            # print(f"[{m_min}][{n_min}:{n_max+1}]")
             spiral.extend(matrix[m_min][n_min:n_max+1])
-            # print(spiral)
-
             m_min += 1
             if m_min > m_max:
                 break
             
-            # print(f"[{m_min}:{m_max+1}][{n_max}]")
             for m in matrix[m_min:m_max+1]:
                 spiral.append(m[n_max])
-            # print(spiral)
-
             n_max -= 1
             if n_min > n_max:
                 break
-
-            # print(f"[{m_max}][{n_min}:{n_max+1}][::-1]")
+            
             spiral.extend(matrix[m_max][n_min:n_max+1][::-1])
-            # print(spiral)
-
             m_max -= 1
             if m_min > m_max:
                 break
             
-            # print(f"[{m_min}:{m_max+1}][::-1][{n_min}]")
             for m in matrix[m_min:m_max+1][::-1]:
                 spiral.append(m[n_min])
-            # print(spiral)
-            
             n_min += 1
-            # print((m_min, m_max), (n_min, n_max))
             if n_min > n_max:
                 break
         return spiral
