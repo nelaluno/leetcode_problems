922e3a246dd2a0f0bf1c4c9ea7b5d6144e0019aa
Anika Filippova <anikax.filippova@intel.com>
2025-12-15 14:38:29 +0200

    Time: 0 ms (100%), Space: 18 MB (78.25%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -7,18 +7,15 @@ class Solution:
         while nums:
             num = nums.pop()
             diff = prev - num
-            # print(prev, num, diff)
             if cur_diff == diff:
                 cur_len += 1
             else:
                 if cur_len >= 2:
                     result += (cur_len-1)*cur_len // 2
                 cur_len = 1
-            # print(cur_len, result)
             prev = num
             cur_diff = diff
         
-        # print(cur_len)
         if cur_len >= 2:
             result += (cur_len-1)*cur_len // 2
         
