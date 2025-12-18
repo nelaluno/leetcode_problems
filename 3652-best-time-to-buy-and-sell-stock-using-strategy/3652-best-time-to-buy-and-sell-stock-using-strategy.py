f4414fac3f3e6c2a445a784a66cff38c72639a41
Anika Filippova <anikax.filippova@intel.com>
2025-12-18 12:27:11 +0200

    Time: 247 ms (85.01%), Space: 30.1 MB (96.41%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -7,17 +7,15 @@ class Solution:
         right_sum = sum([prices[j]*strategy[j] for j in range(k, n)])
         mid_sum = sum(prices[half_k:k])
         for i in range(n-k+1):
-            # print(left_sum, mid_sum, right_sum)
-            last = i+k
             profit = left_sum + right_sum + mid_sum
-            
+            max_profit = max(max_profit, profit)
+
+            last = i+k
             if last != n:
                 right_sum -= prices[last] * strategy[last]
                 mid_sum += prices[last]
 
-            left_sum += prices[i] * strategy[i]
-            mid_sum -= prices[half_k+i]
-            # print(prices[half_k+i], prices[last] if last != n else None,  mid_sum)
-            max_profit = max(max_profit, profit)
+                left_sum += prices[i] * strategy[i]
+                mid_sum -= prices[half_k+i]
 
         return max_profit
