d256128310382577374d8a5737d8ca2e4d0af647
Anika Filippova <anikax.filippova@intel.com>
2026-01-06 17:56:04 +0200

    Time: 651 ms (32.68%), Space: 19.7 MB (5.23%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.py
+++ 1895-largest-magic-square/1895-largest-magic-square.py
@@ -1,6 +1,5 @@
 class Solution:
     def check_sum(self, split, goal):
-        # print(split, goal)
         cur_sum = 0
         for num in split:
             cur_sum += num
@@ -11,7 +10,6 @@ class Solution:
 
     def is_magic_squere(self, grid, i_start, j_start, k):
         summ = sum(grid[i_start][j_start:j_start+k])
-        # print(i_start, j_start, k, summ)
         # all rows 
         for i in range(i_start, i_start+k):
             cur_sum = 0
@@ -24,7 +22,7 @@ class Solution:
         if not self.check_sum([grid[i_start + k - i - 1][j_start + i] for i in range(k)], summ):
             return False
         
-        # all colomns
+        # all columns
         for j in range(j_start, j_start + k):
             if not self.check_sum([grid[i][j] for i in range(i_start, i_start + k)], summ):
                 return False
