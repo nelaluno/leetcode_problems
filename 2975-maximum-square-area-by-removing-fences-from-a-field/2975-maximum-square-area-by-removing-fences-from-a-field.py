806d2dd8a7765df29e3f40d7da83c425025e93e4
Anika Filippova <anikax.filippova@intel.com>
2026-01-16 14:22:04 +0200

    Time: 1611 ms (18.29%), Space: 40.6 MB (77.44%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
@@ -13,13 +13,18 @@ class Solution:
             return (n-1) ** 2
         
         h_diffs = self.get_diffs(hFences, m)
-        v_diffs = self.get_diffs(vFences, n)
-        
-        square_sides =  h_diffs.intersection(v_diffs)
-        if not square_sides:
+
+        vFences.extend([1, n])
+        max_sq_side = 0
+        for i in range(len(vFences)):
+            for j in range(i+1, len(vFences)):
+                value = abs(vFences[j] - vFences[i])
+                if value in h_diffs:
+                    max_sq_side = max(value, max_sq_side)
+
+        if not max_sq_side:
             return -1
     
-        square_sides = max(square_sides)
-        return (square_sides * square_sides) % (10**9 + 7)
+        return (max_sq_side * max_sq_side) % (10**9 + 7)
 
         
