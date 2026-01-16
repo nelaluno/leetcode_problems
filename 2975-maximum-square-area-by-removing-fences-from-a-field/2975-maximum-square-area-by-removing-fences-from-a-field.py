255d353f61ab372d791b3fd9d4485720c9e87087
Anika Filippova <anikax.filippova@intel.com>
2026-01-16 14:24:32 +0200

    Time: 1258 ms (83.54%), Space: 40.1 MB (78.66%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
@@ -2,12 +2,12 @@ MOD = 10**9 + 7
 
 class Solution:
     def get_diffs(self, nums, n):
-        nums.extend([1, n])
+        nums = [1] + sorted(nums) + [n]
 
         diffs = set()
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                diffs.add(abs(nums[j] - nums[i]))
+                diffs.add(nums[j] - nums[i])
         return diffs
 
     def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
@@ -16,11 +16,11 @@ class Solution:
         
         h_diffs = self.get_diffs(hFences, m)
 
-        vFences.extend([1, n])
+        vFences = [1] + sorted(vFences) + [n]
         max_sq_side = 0
         for i in range(len(vFences)):
             for j in range(i+1, len(vFences)):
-                value = abs(vFences[j] - vFences[i])
+                value = vFences[j] - vFences[i]
                 if value in h_diffs:
                     max_sq_side = max(value, max_sq_side)
 
