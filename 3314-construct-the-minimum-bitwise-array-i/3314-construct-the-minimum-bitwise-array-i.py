53a5670d566b717aacef2a36da1fef2ef3720c62
Anika Filippova <anikax.filippova@intel.com>
2026-01-21 13:02:07 +0200

    Time: 132 ms (50.73%), Space: 19.4 MB (18.12%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -3,10 +3,9 @@ class Solution:
         result = []
         for num in nums:
             candidate = -1
-            if num % 2:
-                for j in range(num):
-                    if (j | (j + 1)) == num:
-                        candidate = j
-                        break
+            for j in range(num):
+                if (j | (j + 1)) == num:
+                    candidate = j
+                    break
             result.append(candidate)
         return result
