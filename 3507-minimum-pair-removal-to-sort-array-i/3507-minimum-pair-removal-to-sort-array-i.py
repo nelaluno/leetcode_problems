8ea7bf0dffbbfd6b17ba4896fedee91072e3341b
Anika Filippova <anikax.filippova@intel.com>
2026-01-22 11:50:18 +0200

    Time: 11 ms (78.95%), Space: 19.4 MB (18.99%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
@@ -4,7 +4,6 @@ def get_removal_count(nums):
 
     min_sum = float("inf")
     min_sum_indx = 0
-    is_sorted = True
     for i in range(len(nums) - 1):
         cur_sum = nums[i] + nums[i+1]
         if cur_sum < min_sum:
