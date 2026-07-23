beabf2d76b148b402ea9cc6c295601d082ab831f
Anika Filippova <anika.golub@gmail.com>
2026-07-23 19:21:38 +0300

    Time: 1209 ms (5.01%), Space: 39.2 MB (11.02%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -4,19 +4,19 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         n = len(nums)
         bad_starts = set()
+
         for i in range(n):
             if nums[i] in bad_starts:
                 continue
-            i_values = set()
-            i_indxs = []
+            
+            i_indxs = {}
             for j in range(i+1, n):
                 if nums[i] < nums[j]:
-                    if nums[j] not in i_values:
-                        i_indxs.append(j)
-                        i_values.add(nums[j])
+                    if nums[j] not in i_indxs:
+                        i_indxs[nums[j]] = j
             
             if len(i_indxs) >= 2:
-                for a, b in combinations(i_indxs, 2):
+                for a, b in combinations(i_indxs.values(), 2):
                     if nums[a] < nums[b]:
                         return True
             bad_starts.add(nums[i])
