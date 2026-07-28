6c3a289bff1cdd72bdff5fcdcd888c85029cc5b2
Anika Filippova <anika.golub@gmail.com>
2026-07-28 17:30:59 +0300

    Time: 7 ms (88.43%), Space: 20.6 MB (51.69%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,17 +1,32 @@
+VOWELS = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
+
+
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        s_list = list(s)
-        vowels = {'a', 'e', 'i', 'o', 'u'}
+        if len(s) == 1:
+            return s
         
-        s_vowels = []
-        for i in range(len(s)):
-            if s_list[i].lower() in vowels:
-                s_vowels.append(s_list[i])
-
-        v_ind = 0
-        for i in range(len(s)):
-            if s_list[i].lower() in vowels:
-                s_list[i] = s_vowels[-v_ind-1]
-                v_ind += 1
+        s_list = list(s)
+        
+        start_ind, end_ind = 0, len(s) - 1
+        is_start_vowel, is_end_vowel = s_list[start_ind] in VOWELS, s_list[end_ind] in VOWELS
+        while end_ind > start_ind:
+            if is_start_vowel:
+                if is_end_vowel:
+                    s_list[start_ind], s_list[end_ind] = s_list[end_ind], s_list[start_ind]
+                    start_ind += 1
+                    end_ind -= 1
+                    is_start_vowel, is_end_vowel = s_list[start_ind] in VOWELS, s_list[end_ind] in VOWELS
+                else:
+                    end_ind -= 1
+                    is_end_vowel = s_list[end_ind] in VOWELS
+            else:
+                if is_end_vowel:
+                    start_ind += 1
+                    is_start_vowel = s_list[start_ind] in VOWELS
+                else:
+                    start_ind += 1
+                    end_ind -= 1
+                    is_start_vowel, is_end_vowel = s_list[start_ind] in VOWELS, s_list[end_ind] in VOWELS
 
         return "".join(s_list)
