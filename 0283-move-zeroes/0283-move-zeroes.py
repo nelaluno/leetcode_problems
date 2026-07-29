1cf09e2edfad536c31fe2438034acb9ffd1311d1
Anika Filippova <anika.golub@gmail.com>
2026-07-29 15:15:49 +0300

    Time: 11 ms (16.88%), Space: 20.5 MB (61.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,16 +5,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zero_count = 0
-        zero_indexes = []
-        for i in range(len(nums)):
+        for i in range(len(nums)):              
             if not nums[i]:
                 zero_count += 1
-                zero_indexes.append(i)
-            elif zero_indexes:
-                nums[zero_indexes[0]] = nums[i]
-                zero_indexes.pop(0)
-                zero_indexes.append(i)
-
-        for i in range(zero_count):
-            nums[-i-1] = 0
+            else:
+                nums[i-zero_count], nums[i] = nums[i], nums[i-zero_count]
+            
         return nums
