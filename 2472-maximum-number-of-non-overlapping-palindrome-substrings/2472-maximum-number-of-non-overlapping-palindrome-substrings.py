2155c6335aab41911b9e173a7d072223e30f674c
Anika Filippova <anika.golub@gmail.com>
2026-09-15 13:58:05 +0300

    Time: 8 ms (63.92%), Space: 19.2 MB (88.66%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -0,0 +1,36 @@
+
+from collections import defaultdict
+
+def is_palindrome(s, i_start, i_end):
+    # print(s[i_start:i_end])
+    for i in range((i_end - i_start + 1) // 2):
+        # print("check:", s[i_start + i], s[i_end - i])
+        if s[i_start + i] != s[i_end - i]:
+            return False
+    return True
+
+
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        if k == 1:
+            return len(s)
+        min_double_count = k // 2
+
+        palindrome_count = 0
+        ch_inds = defaultdict(list)
+        double_count = 0
+        for i, ch in enumerate(s):
+            # print(ch, i, ch_inds)
+            ch_inds[ch].append(i)
+            double_count += (len(ch_inds[ch]) % 2 == 0)
+            if len(ch_inds[ch]) > 1 and double_count >= min_double_count:
+                for i_start in ch_inds[ch][:-1]:
+                    if (i - i_start + 1) < k:
+                        break
+
+                    if is_palindrome(s, i_start, i):
+                        palindrome_count += 1
+                        double_count = 0
+                        ch_inds = defaultdict(list)
+                        break
+        return palindrome_count
